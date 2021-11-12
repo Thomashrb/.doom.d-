2715f689ffff4167324eb495cb65796687db4dcb
Thomas <thomas@beat.no>
2021-11-12 01:58:35 +0100

    pony.el: add :config to ponylang mode

--- +pony.el
+++ +pony.el
@@ -6,6 +6,14 @@
 (use-package! ponylang-mode
   :after
   (flycheck-pony)
+  :config
+  (setq ponylang-use-ctags t)
+  (setq ponylang-format-on-save t)
+  (add-hook
+   'ponylang-mode-hook
+   (lambda ()
+     (set-variable 'indent-tabs-mode nil)
+     (set-variable 'tab-width 2)))
   :bind
   (:map ponylang-mode-map
         ("C-c C-v" . ponylang-project-build)
@@ -15,6 +23,5 @@
 (use-package! flycheck-pony
   :config
   (setq create-lockfiles nil))
-
 
 (provide '+pony)
